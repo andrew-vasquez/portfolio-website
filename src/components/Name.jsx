b2203d740114110a054cc23516d00592aecf6d16
Andrew <103331877+Allials@users.noreply.github.com>
2025-05-29 15:34:18 -0500

    changed front-end to full-stack developer

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -3,7 +3,7 @@ const Name = () =>{
     <>
       <div className="flex flex-col items-center justify-center mt-20 mb-16">
         <h2 className="text-4xl font-bold block">Andrew Vasquez</h2>
-            <p className="text-2xl">Front-End Developer</p>
+            <p className="text-2xl">Full-Stack Developer</p>
       </div>
     </>
   );
