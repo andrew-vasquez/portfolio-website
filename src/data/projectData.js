151ab4879a4ac1dfcaf6d31053e258d3a1395a3d
Andrew <103331877+Allials@users.noreply.github.com>
2025-07-25 18:11:47 -0500

    updated project data including daylybread website

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -1,24 +1,35 @@
-import reactLogo from "../assets/icons/react.svg"
-import tailwindLogo from "../assets/icons/tailwind.svg"
-import typescript from "../assets/icons/typescript.svg"
-import terrenos from "../assets/projects/terrenos_oaxaca.png" 
-import pokemon from "../assets/projects/poketeambuilder.png"
+import reactLogo from "../assets/icons/react.svg";
+import tailwindLogo from "../assets/icons/tailwind.svg";
+import typescript from "../assets/icons/typescript.svg";
+import terrenos from "../assets/projects/terrenos_oaxaca.png";
+import pokemon from "../assets/projects/poketeambuilder.png";
+import daylybread from "../assets/projects/daylybread.png";
 export const projectData = [
-    {
-            title: "ALFA Inmobiliaria",
-            description: "Landing page for a real estate company in Oaxaca, Mexico that shows off listings for sale.",
-            image: terrenos,
-            icons: [reactLogo, typescript, tailwindLogo],
-            iconName: ["React", "TypeScript", "Tailwind"],
-            link: "https://alfa-inmobiliaria.vercel.app/"
-        },
-        {
-          title: "Pokemon Team Builder",
-          description: "A team builder that lets you build a team for a specific mainline Pokemon game and see their strengths and weaknesses.",
-          image: pokemon,
-          icons: [reactLogo,tailwindLogo],
-          iconName: ["React", "Tailwind"],
-          link: "https://poketeambuild.netlify.app/"
-        },
-     
-]
+  {
+    title: "DaylyBread",
+    description:
+      "An e-commerce website that sells nfc tap devices to churches and other organizations.",
+    image: daylybread,
+    icons: [reactLogo, typescript, tailwindLogo],
+    iconName: ["React", "TypeScript", "Tailwind"],
+    link: "https://daylybread-page.vercel.app/",
+  },
+  {
+    title: "ALFA Inmobiliaria",
+    description:
+      "Landing page for a real estate company in Oaxaca, Mexico that shows off listings for sale.",
+    image: terrenos,
+    icons: [reactLogo, typescript, tailwindLogo],
+    iconName: ["React", "TypeScript", "Tailwind"],
+    link: "https://alfa-inmobiliaria.vercel.app/",
+  },
+  {
+    title: "Pokemon Team Builder",
+    description:
+      "A team builder that lets you build a team for a specific mainline Pokemon game and see their strengths and weaknesses.",
+    image: pokemon,
+    icons: [reactLogo, tailwindLogo],
+    iconName: ["React", "Tailwind"],
+    link: "https://poketeambuild.netlify.app/",
+  },
+];
